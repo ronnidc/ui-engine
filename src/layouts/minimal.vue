b1bd37ceac43eb8d4c78860c7afc180d01df467e
ronnidc (RDC) <ronnidc@gmail.com>
2020-02-22 11:12:50 +0100

    styles: add new minimal layout (the old default layout)

---
+++ src/layouts/minimal.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="layout-theme">
+        <h2 class="layout-title">Minimal Layout (old default)</h2>
+        <nuxt />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @import "./minimal.scss";
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,6 +14,7 @@
     import Navigation from "~/components/Navigation/Navigation.vue";
 
     export default {
+        layout: 'minimal',
         components: {
             Logo, 
             Navigation
