cba372b5d153100dd00b385533671a7a45698172
ronnidc (RDC) <ronnidc@gmail.com>
2020-02-22 11:14:04 +0100

    styles: move laout styles to scoped seperate file

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,26 +1,10 @@
 <template>
-    <div>
+    <div class="layout-theme">
+        <h2 class="layout-title">Default Template</h2>
         <nuxt />
     </div>
 </template>
 
-<style lang="scss">
-    html {
-        font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-            Roboto, 'Helvetica Neue', Arial, sans-serif;
-        font-size: 16px;
-        word-spacing: 1px;
-        -ms-text-size-adjust: 100%;
-        -webkit-text-size-adjust: 100%;
-        -moz-osx-font-smoothing: grayscale;
-        -webkit-font-smoothing: antialiased;
-        box-sizing: border-box;
-    }
-
-    *,
-    *:before,
-    *:after {
-        box-sizing: border-box;
-        margin: 0;
-    }
+<style lang="scss" scoped>
+    @import "./default.scss";
 </style>
