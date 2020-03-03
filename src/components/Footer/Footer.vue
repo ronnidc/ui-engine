ff6df3c50b9f443b5bd0a8997a907d664ff499d4
ronnidc (RDC) <ronnidc@gmail.com>
2020-03-03 17:39:07 +0100

    feature: add some more components

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="footer">
+        <div>
+            <h4>Page Footer</h4>
+            <p>Content for the footer</p>
+        </div>
+        <ul>
+            <li><a href="#">LinkedIn</a></li>
+            <li><a href="#">YouTube</a></li>
+            <li><a href="#">Twitter</a></li>
+        </ul>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+    @import "./footer.scss";
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="header">
+        <Logo />
+        <Navigation />
+    </header>
+</template>
+
+<script>
+    import Logo from "~/components/Logo/Logo.vue";
+    import Navigation from "~/components/Navigation/Navigation.vue";
+
+    export default {
+        components: {
+            Logo, 
+            Navigation
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    @import "./header.scss";
+</style>
