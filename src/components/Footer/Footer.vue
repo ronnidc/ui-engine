aa9e09ad4ecd767eeee7b80f2888ae349619b4bd
ronnidc (RDC) <ronnidc@gmail.com>
2020-03-03 22:03:06 +0100

    content: add links to footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,13 +1,14 @@
 <template>
     <footer class="footer">
         <div>
-            <h4>Page Footer</h4>
-            <p>Content for the footer</p>
+            <h4>NUXT.JS</h4>
+            <p>Vue.js Meta Framework to create complex, fast & universal web applications quickly.</p>
         </div>
         <ul>
-            <li><a href="#">LinkedIn</a></li>
-            <li><a href="#">YouTube</a></li>
-            <li><a href="#">Twitter</a></li>
+            <li><a href="https://www.vuemastery.com/?ref=nuxt" target="_blank" rel="noreferrer">Vue Mastery</a></li>
+            <li><a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noreferrer">Nuxt Github</a></li>
+            <li><a href="https://github.com/ronnidc/vue-engine" target="_blank" rel="noreferrer">Repository for this site</a></li>
+            <li><a href="https://template.nuxtjs.org/" target="_blank" rel="noreferrer">Play with Nuxt online</a></li>
         </ul>
     </footer>
 </template>
