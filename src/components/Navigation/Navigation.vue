8d74e18e6693499182c1aaf06046a015e217a3bb
ronnidc (RDC) <ronnidc@gmail.com>
2020-03-07 01:20:15 +0100

    Playing with adding an external script to a component

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,21 +1,37 @@
 <template>
     <nav class="navigation">
         <Button text="Frontpage" url="/" theme="primary" />
-        <Button text="Semler Tech" url="/semler" theme="primary" />
+        <Button :text="message" url="/semler" theme="primary" />
         <Button text="Other" url="/other" theme="primary" />
         <Button text="Why" url="/why" theme="primary" />
+        <Button text="Function" url="#" v-on:click="getNavigation" theme="secondary" />
         <Button text="Documentation" url="https://nuxtjs.org/" theme="secondary" target="_blank" rel="noreferrer" />
+        <a href="#" v-on:click="getNavigation">function</a>
     </nav>
 </template>
 
 <script>
+    import { navigation } from './navigation.js';
     import Button from "~/components/Button/Button.vue";
 
     export default {
         components: {
             Button
+        }, 
+        data () {
+            return {
+                // Data can also be asigned here
+                message: 'Morten Semler'
+            }
+        }, 
+        methods: {
+            getNavigation: function() {
+                // Output script from navigation.js
+                navigation();
+            }
         }
     };
+    
 </script>
 
 <style lang="scss">
---
+++ src/components/Navigation/navigation.js
@@ -0,0 +1,14 @@
+export function navigation() {
+    alert('Hello from navigation.js');
+}
+
+
+
+
+// export default {
+//     methods: {
+//       commonMethod() {
+//         console.log('Hello from navigation.js');
+//       }
+//     }
+//   }
