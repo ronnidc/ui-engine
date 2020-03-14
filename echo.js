41c1065295e20716360dd50864ad82565dcbd8d9
ronnidc (RDC) <ronnidc@gmail.com>
2020-03-14 22:01:41 +0100

    Config: optimize & simplify config settings add colored npm script  messages add clean dist script update nuxt version

---
+++ echo.js
@@ -0,0 +1,19 @@
+const args = process.argv;    
+const mssg = args[2];
+
+const opts = [
+  '-s', '--set',
+  '-b', '--bg-color',
+  '-f', '--font-color'];
+
+function excapeAnsiCode(code) {
+  return '\x1b[' + code + 'm';
+}
+
+const ansiStyles = opts.map(function (opt) {
+  return args.indexOf(opt) > -1
+      ? excapeAnsiCode(args[args.indexOf(opt) +1])
+      : '';
+});
+
+console.log('%s%s%s', ansiStyles.join(''), mssg, '\x1b[0m');
