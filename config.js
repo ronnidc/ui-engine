2467cd0c667f3cd148d9bcb24fc29c7d0ab43447
ronnidc (RDC) <ronnidc@gmail.com>
2020-03-14 22:26:51 +0100

    Config: optimize & simplify config settings add config .js file to separate BASE_PATHs from nuxt.config

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
+// Base-path used during development while building prototypes:
 export const BASE_PATH = '/';
-export const PRODUCTION_BASE_PATH = '/poc/ui-engine/';
+
+// Base-path used when exporting the prototype with `npm run generate`:
+export const PRODUCTION_BASE_PATH = '/';
