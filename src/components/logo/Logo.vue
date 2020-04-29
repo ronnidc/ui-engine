0add472f4e9207cdb458a3321efc90832c72b1df
ronnidc (RDC) <ronnidc@gmail.com>
2020-04-30 00:23:04 +0200

    architecture: move logo i static assets directory OPG-3661

--- src/components/logo/Logo.vue
+++ src/components/logo/Logo.vue
@@ -1,6 +1,7 @@
 <template>
-    <img src="./logo.svg" alt="ui-engine logo" class="logo" />
+    <img src="/images/logo.svg" alt="ui-engine logo" class="logo" />
 </template>
+
 
 <style lang="scss">
     @import "./logo.scss";
