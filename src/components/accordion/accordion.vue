02de9172fb9fb327d963e3078dc0bb9590045a87
ronnidc (RDC) <ronnidc@gmail.com>
2020-04-30 10:14:03 +0200

    packages: accordion & google-maps-api

---
+++ src/components/accordion/accordion.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click="isOpen = !isOpen">
+      Toggle
+    </button>
+    
+    <collapse-transition>
+      <div v-show="isOpen">
+        This div will open and close smoothly!
+      </div>
+    </collapse-transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isOpen = false, // closed by default
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+    @import "./accordion.scss";
+</style>
