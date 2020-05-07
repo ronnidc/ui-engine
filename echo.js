d7a01dbd9bee8c394bab23ad2ac07e6e3915cf22
Ronni Chidekel <--help>
2020-05-07 18:11:12 +0200

    optimize build colors

--- echo.js
+++ echo.js
@@ -40,5 +40,26 @@ console.log('%s%s%s', ansiStyles.join(''), mssg, '\x1b[0m');
 │  96   Light Cyan        │
 │  97   White Cyan        │
 └─────────────────────────┘
+┌─────────────────────────┐
+│ Code  Background Color  │
+├─────────────────────────┤
+│  49   Default           │
+│  40   Black             │
+│  41   Red               │
+│  42   Green             │
+│  43   Yellow            │
+│  44   Blue              │
+│  45   Magenta           │
+│  46   Cyan              │
+│  47   Light Gray        │
+│  100  Dark Gray         │
+│  101  Light Red         │
+│  102  Light Green       │
+│  103  Light Yellow      │
+│  104  Light Blue        │
+│  105  Light Magenta     │
+│  106  Light Cyan        │
+│  107  White Cyan        │
+└─────────────────────────┘
 https://stackoverflow.com/a/50275198/1139704
 */
