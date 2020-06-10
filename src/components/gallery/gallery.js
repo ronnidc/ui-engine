7439552207eba9f0ccd9068b065a4724cb91440f
Ronni Chidekel <--help>
2020-06-10 16:51:51 +0200

    remove unused test

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -2,10 +2,5 @@ export function gallery() {
     alert('Hello at The Gallery');
     console.log('Thank you very much for clicking on the Gallery alert box ok button');
 }
-
-export function testtesttest() {
-    alert(testingtesting);
-}
 // Make it global: 
 window.gallery = gallery;
-window.testtesttest = testtesttest;
